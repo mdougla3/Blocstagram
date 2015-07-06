ae9fbd4d73de5408a7343da2bb70c410d92b2c2d
mdougla3 <McCay@McCays-MBP.attlocal.net>
2015-07-06 16:24:21 -0500

    Added in ability to delete photos from the array

---
+++ Blocstgram/ImagesTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  ImagesTableViewController.h
+//  Blocstgram
+//
+//  Created by McCay Barnes on 7/6/15.
+//  Copyright (c) 2015 McCay Barnes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+
+@property (nonatomic, strong) NSMutableArray *images;
+
+@end
