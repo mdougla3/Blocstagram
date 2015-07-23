2d833ef17651446f37daa8be2ceb18a9fcd2aa7e
mdougla3 <McCay@McCays-MBP.attlocal.net>
2015-07-22 20:24:11 -0500

    Pre-work

--- Blocstgram/Blocstgram/CameraViewController.m
+++ Blocstgram/Blocstgram/CameraViewController.m
@@ -8,8 +8,10 @@
 
 #import "CameraViewController.h"
 #import "UIImage+ImageUtilities.h"
+#import "CropBox.h"
+ #import "ImageLibraryViewController.h"
 
-@interface CameraViewController () <CameraToolbarDelegate>
+@interface CameraViewController () <CameraToolbarDelegate, ImageLibraryViewControllerDelegate>
 
 @property (nonatomic, strong) UIView *imagePreview;
 
@@ -17,8 +19,7 @@
 @property (nonatomic, strong) AVCaptureVideoPreviewLayer *captureVideoPreviewLayer;
 @property (nonatomic, strong) AVCaptureStillImageOutput *stillImageOutput;
 
-@property (nonatomic, strong) NSArray *horizontalLines;
-@property (nonatomic, strong) NSArray *verticalLines;
+@property (nonatomic, strong) CropBox *cropBox;
 @property (nonatomic, strong) UIToolbar *topView;
 @property (nonatomic, strong) UIToolbar *bottomView;
 
@@ -43,6 +44,7 @@
     self.imagePreview = [UIView new];
     self.topView = [UIToolbar new];
     self.bottomView = [UIToolbar new];
+    self.cropBox = [CropBox new];
     self.cameraToolbar = [[CameraToolbar alloc] initWithImageNames:@[@"rotate", @"road"]];
     self.cameraToolbar.delegate = self;
     UIColor *whiteBG = [UIColor colorWithWhite:1.0 alpha:.15];
@@ -53,9 +55,7 @@
 }
 
 -(void) addViewsToViewHierarchy {
-    NSMutableArray *views = [@[self.imagePreview, self.topView, self.bottomView] mutableCopy];
-    [views addObjectsFromArray:self.horizontalLines];
-    [views addObjectsFromArray:self.verticalLines];
+    NSMutableArray *views = [@[self.imagePreview, self.topView, self.bottomView, self.cropBox] mutableCopy];
     [views addObject:self.cameraToolbar];
     
     for (UIView *view in views) {
@@ -66,20 +66,6 @@
 -(void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
--(NSArray *) horizontalLines {
-    if (!_horizontalLines) {
-        _horizontalLines = [self newArrayOfFourWhiteViews];
-    }
-    return _horizontalLines;
-}
-
--(NSArray *) verticalLines {
-    if (!_verticalLines) {
-        _verticalLines = [self newArrayOfFourWhiteViews];
-    }
-    return _verticalLines;
 }
 
 -(NSArray *) newArrayOfFourWhiteViews {
@@ -162,22 +148,7 @@
     CGFloat heightOfBottomView = CGRectGetHeight(self.view.frame) - yOriginOfBottomView;
     self.bottomView.frame = CGRectMake(0, yOriginOfBottomView, width, heightOfBottomView);
     
-    CGFloat thirdOfWidth = width / 3;
-    
-    for (int i = 0; i < 4; i++) {
-        UIView *horizontalLine = self.horizontalLines[i];
-        UIView *verticalLine = self.verticalLines[i];
-        
-        horizontalLine.frame = CGRectMake(0, (i * thirdOfWidth) + CGRectGetMaxY(self.topView.frame), width, 0.5);
-        
-        CGRect verticalFrame = CGRectMake(i * thirdOfWidth, CGRectGetMaxY(self.topView.frame), 0.5, width);
-        
-        if (i == 3) {
-            verticalFrame.origin.x -= 0.5;
-        }
-        
-        verticalLine.frame = verticalFrame;
-    }
+    self.cropBox.frame = CGRectMake(0, CGRectGetMaxY(self.topView.frame), width, width);
     
     self.imagePreview.frame = self.view.bounds;
     self.captureVideoPreviewLayer.frame = self.imagePreview.bounds;
@@ -224,8 +195,9 @@
 }
 
 -(void) rightButtonPressedOnToolbar:(CameraToolbar *)toolbar {
-    NSLog(@"Photo library button pressed.");
-}
+    ImageLibraryViewController *imageLibraryVC = [[ImageLibraryViewController alloc] init];
+    imageLibraryVC.delegate = self;
+    [self.navigationController pushViewController:imageLibraryVC animated:YES];}
 
 -(void) cameraButtonPressedOnToolbar:(CameraToolbar *)toolbar {
     AVCaptureConnection *videoConnection;
@@ -249,17 +221,7 @@
             NSData *imageData = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageSampleBuffer];
             UIImage *image = [UIImage imageWithData:imageData scale:[UIScreen mainScreen].scale];
             
-            // #12
-            UIView *leftLine = self.verticalLines.firstObject;
-            UIView *rightLine = self.verticalLines.lastObject;
-            UIView *topLine = self.horizontalLines.firstObject;
-            UIView *bottomLine = self.horizontalLines.lastObject;
-            
-            CGRect gridRect = CGRectMake(CGRectGetMinX(leftLine.frame),
-                                         CGRectGetMinY(topLine.frame),
-                                         CGRectGetMaxX(rightLine.frame) - CGRectGetMinX(leftLine.frame),
-                                         CGRectGetMinY(bottomLine.frame) - CGRectGetMinY(topLine.frame));
-            
+            CGRect gridRect = self.cropBox.frame;
             image = [image imageByScalingToSize:self.captureVideoPreviewLayer.bounds.size andCroppingWithRect:gridRect];
             
             // #13
@@ -278,6 +240,12 @@
             
         }
     }];
+}
+
+#pragma mark - ImageLibraryViewControllerDelegate
+
+-(void) imageLibraryViewController:(ImageLibraryViewController *)imageLibraryViewController didCompleteWithImage:(UIImage *)image {
+    [self.delegate cameraViewController:self didCompleteWithImage:image];
 }
 
 @end
---
+++ Blocstgram/Blocstgram/CropBox.h
@@ -0,0 +1,13 @@
+//
+//  CropBox.h
+//  Blocstgram
+//
+//  Created by McCay Barnes on 7/22/15.
+//  Copyright (c) 2015 McCay Barnes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CropBox : UIView
+
+@end
